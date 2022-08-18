fdec632db94f549cc4c104c9b8ee2f549aae7e7d
Notepad96 <notepad1029@gmail.com>
2022-08-19 02:10:20 +0900

    Add: selected color file

--- TabLayout/app/src/main/java/com/notepad96/tablayout/MainActivity.kt
+++ TabLayout/app/src/main/java/com/notepad96/tablayout/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.notepad96.tablayout
 
-import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.notepad96.tablayout.databinding.ActivityMainBinding
 
@@ -23,26 +20,6 @@ class MainActivity : AppCompatActivity() {
             tab.text = tabTextList[pos]
             tab.setIcon(tabIconList[pos])
         }.attach()
-
-        binding.tabLayout01.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                var iconColor = ContextCompat.getColor(baseContext, R.color.select)
-                tab?.icon?.setColorFilter( iconColor, PorterDuff.Mode.SRC_IN)
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                var iconColor = ContextCompat.getColor(baseContext, R.color.unselect)
-                tab?.icon?.setColorFilter( iconColor, PorterDuff.Mode.SRC_IN)
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                var iconColor = ContextCompat.getColor(baseContext, R.color.select)
-                tab?.icon?.setColorFilter( iconColor, PorterDuff.Mode.SRC_IN)
-            }
-        })
-
-        binding.tabLayout01.selectTab(binding.tabLayout01.getTabAt(0))
 
     }
 
--- TabLayout/app/src/main/java/com/notepad96/tablayout/Page01.kt
+++ TabLayout/app/src/main/java/com/notepad96/tablayout/Page01.kt
@@ -1,10 +1,10 @@
 package com.notepad96.tablayout
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.notepad96.tablayout.databinding.FragmentPage01Binding
 
 class Page01 : Fragment() {
