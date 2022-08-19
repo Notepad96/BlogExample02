8ba55a5e89b6eaa0040c6f4803011401617e53b4
Notepad96 <notepad1029@gmail.com>
2022-08-19 22:49:21 +0900

    Add: Circular Scrolling

--- TabLayout/app/src/main/java/com/notepad96/tablayout/MainActivity.kt
+++ TabLayout/app/src/main/java/com/notepad96/tablayout/MainActivity.kt
@@ -2,6 +2,7 @@ package com.notepad96.tablayout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.notepad96.tablayout.databinding.ActivityMainBinding
 
@@ -20,5 +21,32 @@ class MainActivity : AppCompatActivity() {
             tab.text = tabTextList[pos]
             tab.setIcon(tabIconList[pos])
         }.attach()
+
+        binding.viewPager01.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            var currentState = 0
+            var currentPos = 0
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                if(currentState == ViewPager2.SCROLL_STATE_DRAGGING && currentPos == position) {
+                    if(currentPos == 0) binding.viewPager01.currentItem = 2
+                    else if(currentPos == 2) binding.viewPager01.currentItem = 0
+                }
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            }
+
+            override fun onPageSelected(position: Int) {
+                currentPos = position
+                super.onPageSelected(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                currentState = state
+                super.onPageScrollStateChanged(state)
+            }
+        })
     }
 }
