6fd5d71822e8fa7b7abe8ddded80a897ce8306cf
Notepad96 <notepad1029@gmail.com>
2022-08-21 22:07:13 +0900

    Ok: Example

--- DataBinding/app/src/main/java/com/notepad96/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/notepad96/databinding/MainActivity.kt
@@ -2,10 +2,18 @@ package com.notepad96.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
+import com.notepad96.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this,  R.layout.activity_main) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val person = Person("Park", 30)
+        binding.p1 = person
     }
 }
