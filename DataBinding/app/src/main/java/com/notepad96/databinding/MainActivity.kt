4258a30e5e49df254786f06bb28ae72fad2542c9
Notepad96 <notepad1029@gmail.com>
2022-08-22 00:52:45 +0900

    Add: Preview

--- DataBinding/app/src/main/java/com/notepad96/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/notepad96/databinding/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.notepad96.databinding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import androidx.core.widget.addTextChangedListener
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.notepad96.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this,  R.layout.activity_main) }
+    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this,  R.layout.activity_main) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- DataBinding/app/src/main/java/com/notepad96/databinding/Person.kt
+++ DataBinding/app/src/main/java/com/notepad96/databinding/Person.kt
@@ -1,3 +1,3 @@
 package com.notepad96.databinding
 
-data class Person(val name: String?, val age: Int)
+data class Person(val name: String, val age: Int)
