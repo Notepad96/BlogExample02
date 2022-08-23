3307c6a5b6ee0f4e1a034a4cf0979bf4ce5592f8
Notepad96 <notepad1029@gmail.com>
2022-08-24 02:20:48 +0900

    OK: Example

--- RecyclerViewLastItem/app/src/main/java/com/notepad96/recyclerviewlastitem/MainActivity.kt
+++ RecyclerViewLastItem/app/src/main/java/com/notepad96/recyclerviewlastitem/MainActivity.kt
@@ -2,10 +2,32 @@ package com.notepad96.recyclerviewlastitem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.notepad96.recyclerviewlastitem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.recyclerView01.apply {
+            adapter = RecyclerAdapter()
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        binding.recyclerView01.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if(!binding.recyclerView01.canScrollVertically(1)
+                    && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    Toast.makeText(applicationContext, "Last Item!!", Toast.LENGTH_SHORT).show()
+                    Log.d("myLog", "Check!")
+                }
+            }
+        })
     }
 }
--- RecyclerViewLastItem/app/src/main/java/com/notepad96/recyclerviewlastitem/RecyclerAdapter.kt
+++ RecyclerViewLastItem/app/src/main/java/com/notepad96/recyclerviewlastitem/RecyclerAdapter.kt
@@ -1,7 +1,29 @@
 package com.notepad96.recyclerviewlastitem
 
-class RecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.notepad96.recyclerviewlastitem.databinding.ItemListBinding
 
-    inner class MyView(val binding: )
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.MyView>() {
 
+    inner class MyView(private val binding: ItemListBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(pos: Int) {
+            binding.mainText.text = "$pos"
+            binding.subText.text = "${pos * pos}"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyView {
+        val view = ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyView(view)
+    }
+
+    override fun onBindViewHolder(holder: MyView, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return 40
+    }
 }
