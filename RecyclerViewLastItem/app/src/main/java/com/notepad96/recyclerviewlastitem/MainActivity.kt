26fad4105d0ef0c7ba044593305da3c077d5546d
Notepad96 <notepad1029@gmail.com>
2022-08-24 02:54:10 +0900

    Add: Preview

--- RecyclerViewLastItem/app/src/main/java/com/notepad96/recyclerviewlastitem/MainActivity.kt
+++ RecyclerViewLastItem/app/src/main/java/com/notepad96/recyclerviewlastitem/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.notepad96.recyclerviewlastitem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.notepad96.recyclerviewlastitem.databinding.ActivityMainBinding
@@ -19,13 +18,13 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(context)
         }
 
+        var count = 0
         binding.recyclerView01.addOnScrollListener(object: RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
                 if(!binding.recyclerView01.canScrollVertically(1)
                     && newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    Toast.makeText(applicationContext, "Last Item!!", Toast.LENGTH_SHORT).show()
-                    Log.d("myLog", "Check!")
+                    Toast.makeText(applicationContext, "Last Item (${count++})", Toast.LENGTH_SHORT).show()
                 }
             }
         })
