3313407b53c1c9bbfcef85d92ad99317d329d439
Notepad96 <notepad1029@gmail.com>
2022-08-23 18:30:58 +0900

    Progress: Header RecyclerView

--- RecyclerViewStickyHeader/app/src/main/java/com/notepad96/recyclerviewstickyheader/MainActivity.kt
+++ RecyclerViewStickyHeader/app/src/main/java/com/notepad96/recyclerviewstickyheader/MainActivity.kt
@@ -2,10 +2,22 @@ package com.notepad96.recyclerviewstickyheader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.notepad96.recyclerviewstickyheader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        binding.recyclerView01.apply {
+            adapter = RecyclerAdapter()
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+
     }
 }
--- RecyclerViewStickyHeader/app/src/main/java/com/notepad96/recyclerviewstickyheader/RecyclerAdapter.kt
+++ RecyclerViewStickyHeader/app/src/main/java/com/notepad96/recyclerviewstickyheader/RecyclerAdapter.kt
@@ -1,24 +1,47 @@
 package com.notepad96.recyclerviewstickyheader
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.notepad96.recyclerviewstickyheader.databinding.ItemListBinding
+import com.notepad96.recyclerviewstickyheader.databinding.ItemStickyBinding
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.MyView>(){
+    companion object {
+        const val VIEW_HEADER = 0
+        const val VIEW_ITEM = 1
+        val NAMES = listOf("Kim", "Lee", "Koo")
+    }
 
-    inner class MyView(val binding: View): RecyclerView.ViewHolder(binding.rootView) {
+    inner class MyView(private val binding: ViewBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            (binding as ItemListBinding).mainText.text = NAMES[position % 3]
+            binding.subText.text = "${position * position}"
+        }
+    }
 
+    override fun getItemViewType(position: Int): Int {
+        return if(position == 0) VIEW_HEADER else VIEW_ITEM
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyView {
-        TODO("Not yet implemented")
+        val view = when(viewType) {
+            VIEW_HEADER -> ItemStickyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            else -> ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        }
+        return MyView(view)
     }
 
     override fun onBindViewHolder(holder: MyView, position: Int) {
-        TODO("Not yet implemented")
+        if(position != 0) {
+            holder.bind(position)
+        }
     }
 
     override fun getItemCount(): Int {
-        return 30
+        return 40
     }
+
+
 }
