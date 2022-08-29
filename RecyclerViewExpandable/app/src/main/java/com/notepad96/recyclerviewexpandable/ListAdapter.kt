7a9f11d76208eb0c4cab2d8b66d6563a32b3c255
Notepad96 <notepad1029@gmail.com>
2022-08-29 23:28:52 +0900

    Update: List Adapter

---
+++ RecyclerViewExpandable/app/src/main/java/com/notepad96/recyclerviewexpandable/ListAdapter.kt
@@ -0,0 +1,62 @@
+package com.notepad96.recyclerviewexpandable
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.notepad96.recyclerviewexpandable.databinding.ItemListBinding
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyView>() {
+    val data = mapOf(
+        "커피" to listOf("아메리카노", "바닐라라떼", "카페라떼", "카페모카"),
+        "프라페" to listOf("녹차프라페", "커피프라페", "쿠키프라페"),
+        "에이드" to listOf("레몬에이드", "자몽에이드"),
+        "음료" to listOf("나랑드", "토레타")
+    )
+
+    inner class MyView(private val binding: ItemListBinding): RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.layout01.setOnClickListener {
+                if(binding.layoutDetail01.visibility == View.VISIBLE) {
+                    binding.layoutDetail01.visibility = View.GONE
+                    binding.layoutBtn01.animate().apply {
+                        duration = 200
+                        rotation(0f)
+                    }
+                } else {
+                    binding.layoutDetail01.visibility = View.VISIBLE
+                    binding.layoutBtn01.animate().apply {
+                        duration = 200
+                        rotation(180f)
+                    }
+                }
+            }
+        }
+
+        fun bind(pos: Int) {
+            binding.textView01.text = data.keys.elementAt(pos)
+            data.values.elementAt(pos).forEach {
+                val view = TextView(binding.root.context).apply {
+                    text = "· $it"
+                    textSize = 20f
+                    setPadding(10, 10, 5, 10)
+                }
+                binding.layoutDetail01.addView(view)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyView {
+        val view = ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyView(view)
+    }
+
+    override fun onBindViewHolder(holder: MyView, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
--- RecyclerViewExpandable/app/src/main/java/com/notepad96/recyclerviewexpandable/MainActivity.kt
+++ RecyclerViewExpandable/app/src/main/java/com/notepad96/recyclerviewexpandable/MainActivity.kt
@@ -2,10 +2,19 @@ package com.notepad96.recyclerviewexpandable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.notepad96.recyclerviewexpandable.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.recycler01.apply {
+            adapter = ListAdapter()
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
     }
 }
