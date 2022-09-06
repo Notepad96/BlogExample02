50fccead0baa2d1c88fd0b6b0252180d8cd9dc19
Notepad96 <notepad1029@gmail.com>
2022-09-06 23:59:29 +0900

    Update: Example

--- AlarmService/app/src/main/java/com/notepad96/alarmservice/MainActivity.kt
+++ AlarmService/app/src/main/java/com/notepad96/alarmservice/MainActivity.kt
@@ -4,15 +4,19 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import com.notepad96.alarmservice.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    lateinit var setting: SharedPreferences
 
     companion object {
         const val REQUEST_CODE = 101
@@ -21,17 +25,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        setting = getSharedPreferences("setting", MODE_PRIVATE)
+        binding.toggleButton01.isChecked = setting.getBoolean("alarm", false)
 
         val alarmManager = binding.root.context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val pendingIntent = Intent(binding.root.context, MyAlarmReceiver::class.java).let {
             it.putExtra("code", REQUEST_CODE)
-            it.putExtra("count", 12)
+            it.putExtra("count", 32)
             PendingIntent.getBroadcast(binding.root.context, REQUEST_CODE, it, 0)
         }
 
-        binding.button01.setOnClickListener {
-            // Case 1: 10초 후 10초 간격으로 Alarm (Min Interval: 1 minute)
+        binding.toggleButton01.setOnCheckedChangeListener { _, b ->
+            setting.edit {
+                putBoolean("alarm", b)
+            }
+
+            if(b) {
+                // 1회성 알림, 10초 후
+                alarmManager.set(
+                    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() + 1000 * 10,
+                    pendingIntent
+                )
+
+                // Case 1: 10초 후 10초 간격으로 Alarm (Min Interval: 1 minute)
 //            alarmManager.setInexactRepeating(
 //                AlarmManager.ELAPSED_REALTIME_WAKEUP,
 //                SystemClock.elapsedRealtime() + 1000 * 10,
@@ -39,7 +56,7 @@ class MainActivity : AppCompatActivity() {
 //                pendingIntent
 //            )
 
-            // Case 2: 10초 후 2분 간격으로 Alarm (Min Interval: 1 minute)
+                // Case 2: 10초 후 2분 간격으로 Alarm (Interval: 2 minute)
 //            alarmManager.setInexactRepeating(
 //                AlarmManager.ELAPSED_REALTIME_WAKEUP,
 //                SystemClock.elapsedRealtime() + 1000 * 10,
@@ -47,30 +64,25 @@ class MainActivity : AppCompatActivity() {
 //                pendingIntent
 //            )
 
-            val alarmTime = Calendar.getInstance().apply {
-                timeInMillis = System.currentTimeMillis()
-                set(Calendar.HOUR_OF_DAY, 6)
-                set(Calendar.MINUTE, 40)
+                // Case 3: 오전 8시 27분 Alarm 생성 (Interval: Day)
+//                val calendar = Calendar.getInstance().apply {
+//                    timeInMillis = System.currentTimeMillis()
+//                    set(Calendar.HOUR_OF_DAY, 8)
+//                    set(Calendar.MINUTE, 27)
+//                }
+//                alarmManager.setInexactRepeating(
+//                    AlarmManager.RTC_WAKEUP,
+//                    calendar.timeInMillis,
+//                    AlarmManager.INTERVAL_DAY,
+//                    pendingIntent
+//                )
+                Toast.makeText(applicationContext, "Start", Toast.LENGTH_SHORT).show()
+                Log.d("myLog", "Start")
+            } else {
+                alarmManager.cancel(pendingIntent)
+                Toast.makeText(applicationContext, "Cancel", Toast.LENGTH_SHORT).show()
+                Log.d("myLog", "Cancel")
             }
-            alarmManager.setInexactRepeating(
-                AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                alarmTime.timeInMillis,
-                AlarmManager.INTERVAL_DAY,
-                pendingIntent
-            )
-
-            // 1회성 알림, 10초 후
-//            alarmManager.set(
-//                AlarmManager.ELAPSED_REALTIME_WAKEUP,
-//                SystemClock.elapsedRealtime() + 1000 * 10,
-//                pendingIntent
-//            )
-            Log.d("myLog", "Start")
-        }
-
-        binding.button02.setOnClickListener {
-            alarmManager.cancel(pendingIntent)
-            Log.d("myLog", "Cancel")
         }
     }
 }
--- AlarmService/app/src/main/java/com/notepad96/alarmservice/MyAlarmReceiver.kt
+++ AlarmService/app/src/main/java/com/notepad96/alarmservice/MyAlarmReceiver.kt
@@ -4,10 +4,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 
 class MyAlarmReceiver: BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
         if(p1?.extras?.get("code") == MainActivity.REQUEST_CODE) {
+            Toast.makeText(p0, "Alarm Start", Toast.LENGTH_SHORT).show()
             var count = p1.getIntExtra("count", 0)
             Log.d("myLog", "$count")
         }
