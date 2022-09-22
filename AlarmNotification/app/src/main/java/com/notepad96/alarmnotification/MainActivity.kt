6c603792375f777ae0a03ed27c3b5daab38058f0
Notepad96 <notepad1029@gmail.com>
2022-09-23 00:49:03 +0900

    Update: Alarm Manager

--- AlarmNotification/app/src/main/java/com/notepad96/alarmnotification/MainActivity.kt
+++ AlarmNotification/app/src/main/java/com/notepad96/alarmnotification/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.notepad96.alarmnotification
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import com.notepad96.alarmnotification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    companion object {
+        const val REQUEST_CODE = 101
+    }
+    object view {
+        lateinit var instance: MainActivity
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        view.instance = this
+
+        val alarmManager = binding.root.context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val pendingIntent = Intent(binding.root.context, MyAlarmReceiver::class.java).let {
+            it.putExtra("code", REQUEST_CODE)
+            it.putExtra("count", 10)
+            PendingIntent.getBroadcast(binding.root.context, REQUEST_CODE, it, 0)
+        }
+
+        binding.switch01.setOnCheckedChangeListener { btn, isCheck ->
+            if(isCheck) {
+                alarmManager.set(
+                    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() + 1000 * 10,
+                    pendingIntent
+                )
+                appendLog("Alarm Start")
+            } else {
+                alarmManager.cancel(pendingIntent)
+                appendLog("Alarm Cancel")
+            }
+        }
+    }
+
+    fun appendLog(str: String) {
+        binding.logText.text = "${binding.logText.text}\n${str}"
     }
 }
---
+++ AlarmNotification/app/src/main/java/com/notepad96/alarmnotification/MyAlarmReceiver.kt
@@ -0,0 +1,19 @@
+package com.notepad96.alarmnotification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+class MyAlarmReceiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        if(p1?.extras?.get("code") == MainActivity.REQUEST_CODE) {
+            Toast.makeText(p0, "Alarm Start", Toast.LENGTH_SHORT).show()
+            var count = p1.getIntExtra("count", 0)
+            Log.d("myLog", "$count")
+
+            MainActivity.view.instance.appendLog("Alarm Check $count")
+        }
+    }
+}
